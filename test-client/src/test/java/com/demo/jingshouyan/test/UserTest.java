f994ec811d8da8e9ffec5a13ad1fbc07379c916f
JingShouyan <290173092@qq.com>
2018-12-14 19:09:31 +0800

    user test create

--- test-client/src/test/java/com/demo/jingshouyan/test/UserTest.java
+++ test-client/src/test/java/com/demo/jingshouyan/test/UserTest.java
@@ -1,17 +1,23 @@
 package com.demo.jingshouyan.test;
 
+import com.github.jingshouyan.crud.bean.C;
 import com.github.jingshouyan.crud.bean.R;
 import com.github.jingshouyan.jdbc.comm.bean.Page;
 import com.github.jingshouyan.jdbc.comm.util.ConditionUtil;
 import com.github.jingshouyan.jrpc.base.bean.Rsp;
+import com.github.jingshouyan.jrpc.base.util.json.JsonUtil;
 import com.github.jingshouyan.jrpc.client.JrpcClient;
 import com.github.jingshouyan.jrpc.client.Request;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.Map;
+import java.util.stream.IntStream;
 
 /**
  * @author jingshouyan
@@ -58,6 +64,38 @@ public class UserTest {
     @Test
     public void limit(){
         query(R.TYPE_LIMIT);
+    }
+
+    @Test
+    public void create2() throws Exception{
+        IntStream.range(0,100).parallel()
+                .forEach(i -> create());
+        Thread.sleep(10000);
+        long start = System.currentTimeMillis();
+        IntStream.range(0,1000).parallel()
+                .forEach(i -> create());
+        long end = System.currentTimeMillis();
+        System.out.println(end - start);
+    }
+
+    @Test
+    public void create() {
+
+        Map<String,Object> m = Maps.newHashMap();
+        m.put("name","这地方");
+        m.put("name2","这地方");
+        m.put("nickname","这地方222");
+        C c = new C();
+        c.setBean("user");
+        c.setType(C.TYPE_SINGLE);
+        c.setData(JsonUtil.toJsonString(m));
+        Rsp rsp = Request.newInstance()
+                .setClient(client)
+                .setServer("user")
+                .setMethod("create")
+                .setParamObj(c)
+                .send();
+        System.out.println(rsp);
     }
 
     private void query(String type){
